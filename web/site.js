e6391d00a1d40222715095c2949caf86bfd184b8
kixelated <kixelated@gmail.com>
2020-01-22 23:15:13 -0800

    Overlay and start of full item display.

--- web/site.js
+++ web/site.js
@@ -47,17 +47,20 @@ class Buyer extends React.Component {
 	constructor(props) {
 		super(props);
 
+		/*
 		this.state = {
 			buyer: props.buyer,
 			step: "SHOW",
 		};
+		*/
 	}
 
 	render() {
-		if (this.state.buyer) {
-			return "Purchased by " + this.state.buyer.S + "!";
-		}
+		if (!this.props.buyer) return null
 
+		return "Purchased by " + this.props.buyer.S + "!";
+
+		/*
 		if (this.state.step == "SHOW") {
 			const onClick = (e) => {
 				this.setState({ step: "FORM" })
@@ -68,43 +71,7 @@ class Buyer extends React.Component {
 		}
 
 		if (this.state.step == "FORM") {
-			return create(BuyForm)
-			/*
-			return create(PaypalButton, {
-				createOrder: () => {
-					return fetch('/create-order', {
-						method: 'post',
-						headers: {
-							'content-type': 'application/json'
-						}
-					}).then((resp) => {
-						return resp.json()
-					}).then((data) => {
-						return data.orderID
-					}).catch((err) => {
-						this.setState({ error: err })
-					})
-				},
-				onApprove: (data) => {
-					this.setState({ step: "APPROVING" })
-					return fetch('/capture-order', {
-						method: 'post',
-						headers: {
-							'content-type': 'application/json',
-						},
-						body: JSON.stringify({
-							orderID: data.orderID,
-						})
-					}).then((resp) => {
-						return resp.json()
-					}).then((details) => {
-						this.setState({ step: "APPROVED" })
-					}).catch((err) => {
-						this.setState({ step: "FORM", error: err })
-					})
-				},
-			})
-			*/
+			//return create(BuyForm)
 		}
 
 		if (this.state.step == "APPROVING") {
@@ -114,6 +81,71 @@ class Buyer extends React.Component {
 		if (this.state.step == "APPROVED") {
 			return "OMG THANKS"
 		}
+		*/
+	}
+}
+
+class FullItem extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		if (!this.props.item) return null
+
+		const img = create('img', { src: this.props.item.Image.S })
+		const name = create('div', { className: "name" }, this.props.item.Name.S)
+		const cost = create('div', { className: "cost" }, "$" + this.props.item.Cost.N)
+		const description = create('div', { className: "description" }, this.props.item.Description.S)
+		const buyer = create(Buyer, { buyer: this.props.item.Buyer })
+
+		const onClick = (e) => {
+			this.props.onDeselect()
+			return false
+		}
+
+		const paypal = create(PaypalButton, {
+			createOrder: () => {
+				return fetch('/create-order', {
+					method: 'post',
+					headers: {
+						'content-type': 'application/json'
+					}
+				}).then((resp) => {
+					return resp.json()
+				}).then((data) => {
+					return data.orderID
+				}).catch((err) => {
+					this.setState({ error: err })
+				})
+			},
+			onApprove: (data) => {
+				this.setState({ step: "APPROVING" })
+				return fetch('/capture-order', {
+					method: 'post',
+					headers: {
+						'content-type': 'application/json',
+					},
+					body: JSON.stringify({
+						orderID: data.orderID,
+					})
+				}).then((resp) => {
+					return resp.json()
+				}).then((details) => {
+					this.setState({ step: "APPROVED" })
+				}).catch((err) => {
+					this.setState({ step: "FORM", error: err })
+				})
+			},
+		})
+
+		const container = create('div', { className: "fullItem" }, img, name, cost, buyer, description, paypal)
+
+		const deselect = create('a', { className: "deselect", href: "#back", onClick: onClick })
+
+		const overlay = create('div', { className: "overlay" }, deselect, container, deselect)
+
+		return overlay
 	}
 }
 
@@ -128,14 +160,14 @@ class RegistryItem extends React.Component {
 		const cost = create('div', { className: "cost" }, "$" + this.props.item.Cost.N)
 
 		//const description = create('div', { className: "description" }, this.props.item.Description.S)
-		//const buy = create(Buyer, { buyer: this.props.item.Buyer })
 
-		//const style = {};
-		//if (this.props.item.Image) {
-		//style.backgroundImage = `url(${this.props.item.Image.S})`
-		//}
+		const onClick = (e) => {
+			this.props.onSelected(this.props.item)
+			return false
+		}
 
-		const link = create('a', { href: "#" }, img, name, cost)
+		const link = create('a', { href: "#", onClick: onClick }, img, name, cost)
+
 		return create('div', { className: "item" }, link);
 	}
 }
@@ -146,6 +178,7 @@ class Registry extends React.Component {
 
 		this.state = {
 			items: [],
+			selected: null,
 		};
 
 		fetch('/items').then((resp) => {
@@ -156,18 +189,30 @@ class Registry extends React.Component {
 	}
 
 	render() {
+		const onSelected = (item) => {
+			this.setState({ selected: item })
+		}
+
+		const overlay = create(FullItem, {
+			item: this.state.selected,
+			onDeselect: onSelected,
+		})
+
 		const items = this.state.items.map((item) => {
 			return create(RegistryItem, {
 				item: item,
 				key: item.Id.S,
+				onSelected: onSelected,
 			})
 		});
 
-		return create('div', { className: "items" }, items)
+		const itemsContainer = create('div', { className: "items" }, items)
+
+		return create('div', { className: "container" }, overlay, itemsContainer)
 	}
 }
 
 const registry = create(Registry)
-const container = document.getElementById('app');
+const container = document.getElementById('app')
 
-ReactDOM.render(registry, container);
+ReactDOM.render(registry, container)
