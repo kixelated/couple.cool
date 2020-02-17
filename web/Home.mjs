19706d69a002e44536a90a1770c3b806d4275846
kixelated <kixelated@gmail.com>
2020-02-16 21:03:06 -0800

    Progeers

--- web/Home.mjs
+++ web/Home.mjs
@@ -1,19 +1,34 @@
+const create = React.createElement
+
 export class Home extends React.Component {
 	render() {
-		return React.createElement("div", { className: "home" },
-			React.createElement("div", { className: "centerpiece" },
-				React.createElement("div", { className: "names" },
-					React.createElement("span", { className: "rebe" }, "Rebe"),
-					React.createElement("span", { className: "plus" }, "+"),
-					React.createElement("span", { className: "luke" }, "Luke"),
+		return create("div", { className: "home" },
+			create("div", { className: "centerpiece" },
+				create("div", { className: "names" },
+					create("img", { className: "bride", src: "images/kixelBride.png" }),
+					create("span", { className: "rebe" }, "Rebe"),
+					create("span", { className: "plus" }, "+"),
+					create("span", { className: "luke" }, "Luke"),
+					create("img", { className: "fancy", src: "images/kixelFancy.png" }),
 				),
-				React.createElement("div", { className: "cool" }, "😎 the cool couple 😎"),
-				React.createElement("div", { className: "date" }, "04.25.2020"),
+				create("div", { className: "cool" }, "😎 the cool couple 😎"),
+				create("div", { className: "date" }, "04.25.2020"),
 			),
-			React.createElement("div", { className: "intro" },
-				React.createElement("p", {}, "So we're getting married. A lifetime of commitment. That's cool I guess."),
-				React.createElement("p", {}, "We're actually very excited! It's not every day that you get a chance to profess your admiration for another individual in front of friends/family. Plus it's a chance to get said friends/family drunk in a park. So come, celebrate the occasion that is our marriage, and the start of the next phase of our life!"),
-				React.createElement("p", {}, "You can view pictures of said cool couple, or perhaps visit our registry and shower us in gifts. So many posibilities!"),
+			create("div", { className: "intro" },
+				create("p", {}, "So we're getting married. A lifetime of commitment. That's cool I guess."),
+				create("p", {}, "We're actually very excited! It's not every day that you get a chance to profess your admiration for another individual in front of friends/family. Plus it's a chance to get said friends/family drunk in REDACTED. So come, celebrate the occasion that is our marriage, and the start of the next phase of our life!"),
+				create("p", {}, 
+					create("img", { src: "images/couple.jpg" })
+				),
+				create("p", {}, 
+					create(React.Fragment, {},
+						"You can ",
+						create("a", { href: "#couple" }, "view pictures"),
+						" of said cool couple, or perhaps ",
+						create("a", { href: "#registry" }, "visit our registry"),
+						" and shower us in gifts. So many posibilities!"
+					),
+				),
 			),
 		)
 	}
